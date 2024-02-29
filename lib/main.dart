3e82f2ca9b44854cad714628e580574c06f695d6
Alvian14 <124866722+Alvian14@users.noreply.github.com>
2024-02-29 11:04:12 +0700

    commit pertama

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+// import 'package:project_kucari/page/login_screen.dart';
+import 'package:project_kucari/page/splash_screen.dart'; // Adjust the package name accordingly
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+      // home: LoginScreen(),
+    );
+  }
+}
+
